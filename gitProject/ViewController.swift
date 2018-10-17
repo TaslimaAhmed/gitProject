96abd395bf540ce819ac4b200507921859dec0a3
MAC-LAB-04 <mac-lab-04@mac-lab-04s-mac-mini.local>
2018-10-17 14:58:43 +0600

    Initial Commit

---
+++ gitProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  gitProject
+//
+//  Created by MAC-LAB-04 on ১৭/১০/১৮.
+//  Copyright © ২০১৮ Digicon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
